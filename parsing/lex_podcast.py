d848a329f0dbc2cbf3d5dc6109559d8b7ea6d007
Andrey <andreypomortsev@gmail.com>
2023-07-12 20:46:49 +0500

    Deleted unused imports (random and time); added from bs4.element import ResultSet; added getLogger; added exception handler to get_duration, to handle ValueError; changed regex patter in get_youtube_id to get youtube ID for a vidoe by link; devide get_data into two separate functions: The get_data() function retrieves podcast episode data from the web page and collects the title, guest name, description, duration, YouTube URL, audio file URL, and thumbnail URL for each episode. The data is returned in the form of a set of tuples, where each tuple contains the metadata for a single podcast episode.
    The parse_the_data() function is used to parse the data for a single episode, it takes a BeautifulSoup ResultSet object as input, then extracts the relevant data and returns a tuple containing the parsed information.
    
    Added main function that calls the get_data function to retrieve all the podcast episodes from the Lex Fridman podcast webpage and passes each episode to the parse_the_data function to extract the relevant metadata for the podcast. Finally, it writes the retrieved metadata to a CSV file using the save_list_to_csv function.

--- parsing/lex_podcast.py
+++ parsing/lex_podcast.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Module parses the lexfridman.com/podcast and return information about all podcasts
-in format of a csv file delimited by a ','.
+in format of a csv file delimited by a ';'.
 
 The csv file consists the next fields:
     - title (str): the title of the episode.
@@ -17,14 +17,13 @@ import csv
 import io
 import logging
 import os
-import random
 import re
-import time as time_to_sleep
 from datetime import datetime
 
 import googleapiclient.discovery
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import ResultSet
 from dotenv import load_dotenv
 from googleapiclient.errors import HttpError
 from mutagen.mp3 import MP3
@@ -32,6 +31,7 @@ from mutagen.mp3 import MP3
 load_dotenv()
 api_key = os.getenv("api_key")
 
+logging.getLogger(__name__)
 logging.basicConfig(
     format="%(asctime)s %(message)s", filename="errors.log", level=logging.ERROR
 )
@@ -52,6 +52,9 @@ def get_description(url: str) -> str:
         requests.exceptions.RequestException: if there is an error
         retrieving the web page from the specified URL.
     """
+    logging.info(
+        "Retrieves the description text of a Lex Fridman podcast episode from: %s", url
+    )
     response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -67,9 +70,8 @@ def get_description(url: str) -> str:
             return result
         result = text_div.find("span").text
         return result
-    except Exception as error:
-        logging.exception("Couldn't find any description in podcast %s, %s", url, error)
-        return None
+    except Exception:
+        return ""
 
 
 def get_duration(mp3_url: str) -> float:
@@ -179,17 +181,15 @@ def convert_to_timestamp(date_str: str) -> tuple:
     try:
         # Parse the input string as a datetime object
         timestamp = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        # Extract the date and time components from the datetime object
+        date = timestamp.date()
+        time = timestamp.time()
+        # Return the date and time components as a tuple
+        return date, time
     except ValueError as error:
         # Raise an error if the input string is not in the expected format
         logging.exception("Invalid ISO 8601 format: %s: %s", date_str, error)
         return 0, 0
-
-    # Extract the date and time components from the datetime object
-    date = timestamp.date()
-    time = timestamp.time()
-
-    # Return the date and time components as a tuple
-    return date, time
 
 
 def get_youtube_id(youtube_url: str) -> str:
@@ -205,11 +205,11 @@ def get_youtube_id(youtube_url: str) -> str:
         is not in the correct format.
     """
     # Search for the video ID in the URL using a regular expression
-    match = re.search(r"youtube\.com\/watch\?v=(\w+)", youtube_url)
+    match = re.search(r"(?<=v=)[\w-]+", youtube_url)
 
     # If a match is found, extract the video ID from the match object
     if match:
-        video_id = match.group(1)
+        video_id = match.group(0)
         return video_id
 
     # If no match is found add the info to the logging, and return None
@@ -230,7 +230,7 @@ def check_url_response(url: str) -> str:
         If the response status code is 200 OK, return the input URL.
         Otherwise, return None.
     """
-    response = requests.get(url, timeout=5)
+    response = requests.get(url, timeout=10)
     if response.status_code == requests.codes.ok:
         return url
     logging.exception("The url %s responce code is %s", url, response.status_code)
@@ -300,11 +300,10 @@ def get_audio_name(thumbnail_url: str, podcast_url: str) -> str:
 
 def get_data() -> set:
     """
-    Retrieves podcast episode data from "https://lexfridman.com/podcast/" and
-    collects the title, guest name, description, duration, YouTube URL,
-    audio file URL, and thumbnail URL for each episode.
+    Retrieves podcast episode data from "https://lexfridman.com/podcast/"
 
     Returns:
+        bs4.element.ResultSet
         A set of tuples, where each tuple contains the metadata for
         a single podcast episode.
         The tuples are structured as follows:
@@ -319,7 +318,6 @@ def get_data() -> set:
             - time (datetime.time): time the podcast was uploaded on youtube.com.
     """
     url = "https://lexfridman.com/podcast/"
-    csv_episodes = set()
     try:
         response = requests.get(url, timeout=5)
     except requests.exceptions.RequestException as error:
@@ -333,42 +331,58 @@ def get_data() -> set:
 
     soup = BeautifulSoup(response.content, "html.parser")
 
-    episods = soup.find_all("div", {"class": "guest"})
-    for episode in episods:
-        time_to_sleep.sleep(round(random.uniform(0.00, 3.00), 2))
-        try:
-            youtube_url, podcast_page = [
-                link["href"] for link in episode.select("div.vid-materials a")[:2]
-            ]
-            title = episode.select_one(".vid-title a").text
-            guest = episode.select_one(".vid-person").text
-            thumbnail_url = episode.select_one(".thumb-youtube img")["src"]
-            description = get_description(podcast_page)
-            audio_file_url = get_audio_name(thumbnail_url, podcast_page)
-            duration = get_duration(audio_file_url)
-            youtube_video_id = get_youtube_id(youtube_url)
-            date, time = get_date_time(youtube_video_id, api_key)
-            record = (
-                title,
-                guest,
-                description,
-                duration,
-                youtube_url,
-                audio_file_url,
-                thumbnail_url,
-                date,
-                time,
-            )
-            duplicate_score = 0
-            if record in csv_episodes:
-                duplicate_score += 0.5
-                if duplicate_score == 1:
-                    return csv_episodes
-            csv_episodes.add(record)
-        except Exception as error:
-            logging.exception("There is %s in %s", error, title)
-            continue
-    return csv_episodes
+    episodes = soup.find_all("div", {"class": "guest"})
+    return episodes
+
+
+def parse_the_data(episode: ResultSet) -> tuple | None:
+    """Parses the data for the given episode and returns a tuple containing
+    the following data:
+    - Title of the episode
+    - Name of the guest(s) of the episode
+    - Description of the episode
+    - Duration of the episode
+    - YouTube URL of the episode
+    - Audio file URL of the episode
+    - Thumbnail URL of the episode
+    - Date of the episode
+    - Time of the episode
+
+    Args:
+    - episode: bs4.element.ResultSet: The episode to parse
+
+    Returns:
+    - tuple | None: A tuple containing the data for the episode or None
+    if an error occurs while parsing the data
+    """
+
+    try:
+        youtube_url, podcast_page = [
+            link["href"] for link in episode.select("div.vid-materials a")[:2]
+        ]
+        title = episode.select_one(".vid-title a").text
+        guest = episode.select_one(".vid-person").text
+        thumbnail_url = episode.select_one(".thumb-youtube img")["src"]
+        description = get_description(podcast_page)
+        audio_file_url = get_audio_name(thumbnail_url, podcast_page)
+        duration = get_duration(audio_file_url)
+        youtube_video_id = get_youtube_id(youtube_url)
+        date, time = get_date_time(youtube_video_id, api_key)
+        record = (
+            title,
+            guest,
+            description,
+            duration,
+            youtube_url,
+            audio_file_url,
+            thumbnail_url,
+            date,
+            time,
+        )
+        return record
+    except Exception as error:
+        logging.exception("There is %s in %s", error, title)
+        return [None for _ in range(9)]
 
 
 def save_list_to_csv(data: list, file_name: str) -> None:
@@ -380,7 +394,7 @@ def save_list_to_csv(data: list, file_name: str) -> None:
         file_name: A string representing the file_name for the output CSV file.
     """
     with open(f"{file_name}.csv", "w", newline="", encoding="UTF-8") as file:
-        writer = csv.writer(file)
+        writer = csv.writer(file, delimiter=";")
 
         # Write header row
         header = (
@@ -401,6 +415,16 @@ def save_list_to_csv(data: list, file_name: str) -> None:
             writer.writerow(row)
 
 
-if __name__ == "__main__":
-    data_list = get_data()
+def main() -> None:
+    """This function calls the get_data function to retrieve all the podcast episodes
+    from the Lex Fridman podcast webpage and passes each episode to the parse_the_data
+    function to extract the relevant metadata for the podcast. Finally, it writes
+    the retrieved metadata to a CSV file using the save_list_to_csv function.
+    """
+    episodes = get_data()
+    data_list = set(map(parse_the_data, episodes))
     save_list_to_csv(data_list, "lex_podcasts")
+
+
+if __name__ == "__main__":
+    main()
