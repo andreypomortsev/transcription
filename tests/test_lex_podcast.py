b211fe4ffd9f950e8dc204639404fea9d1dcada3
Andrey <andreypomortsev@gmail.com>
2023-07-13 14:46:01 +0500

    Add classmethod to read test_data from fixtures to test the lex_podcast.py to the variable PODCASTS_DATA, tested the convert_to_timestamp functions with sad and good paths.

--- tests/test_lex_podcast.py
+++ tests/test_lex_podcast.py
@@ -1,19 +1,52 @@
-import unittest
-import random
-from parsing import *
+"""
+Test Cases for Mocking Lab
+"""
+import csv
+import datetime
+from unittest import TestCase
+from unittest.mock import Mock, patch
 
-class TestLexScrapping(unittest.TestCase):
-    def setUp(self) -> None:
-        # Download a random data from fixtures
-        self.rand = random.randint(0, 100)
-        return super().setUp()
-    def tearDown(self) -> None:
-        return super().tearDown()
+from requests import Response
+
+from parsing import lex_podcast
+
+PODCASTS_DATA = {}
+
+class TestLexPodcastParser(TestCase):
+    """Tests Cases for Lex Podcasts parser"""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """ Load test_data responses needed by tests """
+        global PODCASTS_DATA
+        with open('tests/fixtures/test_data.csv') as csv_data:
+            reader = csv.reader(csv_data, delimiter=";")
+            PODCASTS_DATA = list(reader)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """ Delete test_data from RAM """
+        global PODCASTS_DATA
+        PODCASTS_DATA = {}
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
     
-    def test_load_api(self):
-        """ Test download the api_key from .env """
-        pass
+    def test_convert_to_timestamp_with_good_path(self):
+        """Test convert_to_timestamp function with a valid input"""
+        date_str = '2022-11-04 16:09:32'
+        expected_date = datetime.date(2022, 11, 4)
+        expected_time = datetime.time(16, 9, 32)
+        self.assertEqual(lex_podcast.convert_to_timestamp(date_str), (expected_date, expected_time))
 
-    def test_get_description(self):
-        """ Test get description from a podcast url """
+    def test_convert_to_timestamp_with_sad_path(self):
+        """Test convert_to_timestamp function with a invalid input"""
+        date_str = '2022-11-04T16:09:32Z'
+        self.assertEqual(lex_podcast.convert_to_timestamp(date_str), (0, 0))
+        self.assertEqual(lex_podcast.convert_to_timestamp(''), (0, 0))
+
+    @patch("")
+    def test_get_youtube_id_valid(self):
+        """Test get_youtube_id with valid link"""
         pass
